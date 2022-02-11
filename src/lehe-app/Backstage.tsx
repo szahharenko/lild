0ccc0384554f28826c9ed13129fbfbf9c52c4bcd
Sergei <acrashik@gmail.com>
2022-02-11 18:12:54 +0200

    tiny fix

--- src/lehe-app/Backstage.tsx
+++ src/lehe-app/Backstage.tsx
@@ -22,13 +22,13 @@ const Backstage: React.FC<Props> = ({ onLeafClick, isPlayingGame, rulesToggle, r
     </div>
     <div className={`leafs ${isPlayingGame ? 'leafs__active' : ''}`}>
       <span className="leaf1-wrapper">
-        <span className={`leaf leaf1 ${currentLeaf === 1 && 'leaf1__active'}`} onClick={ () => onLeafClick(1) }></span>
+        <span className={`leaf leaf1 ${currentLeaf === 1 ? 'leaf1__active' : ''}`} onClick={ () => onLeafClick(1) }></span>
       </span>
       <span className="leaf2-wrapper">
-        <span className={`leaf leaf2 ${currentLeaf === 2 && 'leaf2__active'}`} onClick={ () => onLeafClick(2) }></span>
+        <span className={`leaf leaf2 ${currentLeaf === 2 ? 'leaf2__active' : ''}`} onClick={ () => onLeafClick(2) }></span>
       </span>
       <span className="leaf3-wrapper">
-        <span className={`leaf leaf3 ${currentLeaf === 3 && 'leaf3__active'}`} onClick={ () => onLeafClick(3) }></span>
+        <span className={`leaf leaf3 ${currentLeaf === 3 ? 'leaf3__active' : ''}`} onClick={ () => onLeafClick(3) }></span>
       </span>
     </div>
     <button onClick={tgl} className='toggle-rules-button button-secondary button-small'>INFO JA REEGLID</button>
